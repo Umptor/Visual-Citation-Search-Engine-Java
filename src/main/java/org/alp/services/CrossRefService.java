a7087390c7a11aa1079c1d0c971c73134aa94b23
Umptor <bongutcha@gmail.com>
2021-06-06 22:04:41 +0300

    what is life

--- src/main/java/org/alp/services/CrossRefService.java
+++ src/main/java/org/alp/services/CrossRefService.java
@@ -3,7 +3,6 @@ package org.alp.services;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import org.alp.models.Paper;
-import org.alp.models.crossrefApi.getMetaDataResponse.GetMetadataResponse;
 import org.alp.models.crossrefApi.getWorksResponse.GetWorksResponse;
 import org.alp.models.crossrefApi.Item;
 import org.alp.models.crossrefApi.Reference;
@@ -21,7 +20,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class CrossRefService {
@@ -48,18 +46,7 @@ public class CrossRefService {
 		return ret;
 	}
 
-	public static Paper getMetadata(Paper paper) throws URISyntaxException {
-		var response = getMetadata(paper.getDoi());
-
-		var responseArr = new ArrayList<CompletableFuture<HttpResponse<String>>>();
-		responseArr.add(response);
-
-		var ret = mapMetadataResponsesToObjects(responseArr).get(0);
-		fixPaperReferences(ret);
-
-		return ret;
-	}
-	private static CompletableFuture<HttpResponse<String>> getMetadata(String doi) throws URISyntaxException {
+	private static CompletableFuture<HttpResponse<String>> getMetadataAsync(String doi) throws URISyntaxException {
 		var httpClient = HttpClient.newHttpClient();
 		String urlString = crossRefUrl + "works/" + URLEncoder.encode(doi, StandardCharsets.UTF_8);
 		urlString = ParamBuilder.addParam(urlString, "mailto", "e160503134@stud.tau.edu.tr");
@@ -105,58 +92,15 @@ public class CrossRefService {
 	 *
 	 */
 	public static ArrayList<Paper> getRelatedPapers(Paper paper, int depth, boolean regetReferences) throws URISyntaxException, InterruptedException {
-		ArrayList<Paper> ret;
-		if(regetReferences) {
-			Paper newPaper = getMetadata(paper);
-			ret = getConnections(newPaper, depth);
-			paper.setReferences(ret);
-		} else {
-			ret = getConnections(paper, depth);
-		}
-		fixPaperReferences(paper);
-		return ret;
+
 	}
 
 	private static ArrayList<Paper> getConnections(Paper paper, int depth) throws URISyntaxException, InterruptedException {
-		// TODO: Only get depth amount of references
-		ArrayList<Paper> references = new ArrayList<>();
-		ArrayList<Paper> existingReferences = new ArrayList<>();
-		ArrayList<CompletableFuture<HttpResponse<String>>> responses = new ArrayList<>();
 
-		if(paper.getReferences() != null && depth > 0) {
-			for(int i = 0; i < paper.getReferences().size(); i++) {
-				String title = paper.getReferences().get(i).getTitle();
-				if(title != null && !title.equals("")) {
-					existingReferences.add(paper.getReferences().get(i));
-				}
-
-				// Wait 1 second between every 20 requests because of the limit on CrossRef's Api. Even though the limit
-				// is 50 requests per second, when I set this to 20+ I get errors
-				if(responses.size() % 20 == 0 && responses.size() != 0) {
-					TimeUnit.SECONDS.sleep(1);
-				}
-				responses.add(getMetadata(paper.getReferences().get(i).getDoi()));
-			}
-
-			// Join response, then map item to paper and add into references
-			references = mapMetadataResponsesToObjects(responses);
-			references.addAll(existingReferences);
-
-			for(Paper reference : references) {
-				paper.setReferences(references);
-				getConnections(reference, depth - 1);
-			}
-		}
-
-		return references;
 	}
 
 	private static ArrayList<Paper> mapMetadataResponsesToObjects(ArrayList<CompletableFuture<HttpResponse<String>>> responses) {
-		return responses.stream()
-				.map(response -> mapItemToPaper(
-						jsonMapperToBody(response.join().body(), GetMetadataResponse.class).getMessage()))
-				.filter((Paper reference) -> reference.getTitle() != null || !reference.getTitle().equals(""))
-				.collect(Collectors.toCollection(ArrayList::new));
+
 	}
 
 	private static <T> T jsonMapperToBody(String str, Class<T> classOfT) {
@@ -173,60 +117,14 @@ public class CrossRefService {
 	}
 
 	private static void fixPaperReferences(Paper root) {
-		fixPaperReferencesAlgo(root, new ArrayList<>());
 	}
 
 	private static void fixPaperReferencesAlgo(Paper root, ArrayList<Paper> visited) {
-		if(visited.contains(root) || root.getReferences() == null) {
-			return;
-		}
 
-		ArrayList<Paper> references = root.getReferences();
-
-		references.forEach(reference -> {
-			visited.add(reference);
-
-			if(reference.getReferences() == null) {
-				reference.setReferences(new ArrayList<>());
-			}
-
-			if(reference.getReferences().contains(root)) return;
-
-			reference.getReferences().add(root);
-
-			fixPaperReferencesAlgo(reference, visited);
-		});
 	}
 
 	public static Paper findPaper(Paper rootPaper, String doiToFind) {
 
-		// BFS Search for paper
-		ArrayList<Paper> currentLevel = new ArrayList<>();
-		ArrayList<Paper> nextLevel = new ArrayList<>();
-		currentLevel.add(rootPaper);
-		if(rootPaper.getDoi().equals(doiToFind)) {
-			return rootPaper;
-		}
-		while(currentLevel.size() > 0) {
-			for(Paper paperInLevel: currentLevel) {
-				if(paperInLevel == null || paperInLevel.getReferences() == null) {
-					continue;
-				}
-				for(Paper neighbour: paperInLevel.getReferences()) {
-					if(neighbour == null) {
-						continue;
-					}
-					if(neighbour.getDoi().equals(doiToFind)) {
-						return neighbour;
-					}
-					nextLevel.add(neighbour);
-				}
-			}
-			currentLevel = new ArrayList<>(nextLevel);
-			nextLevel.clear();
-		}
-		System.out.println("Paper not found, huh?");
-		return new Paper();
 	}
 }
 
--- src/main/java/org/alp/services/graphstream/PaperCoordinateGiver.java
+++ src/main/java/org/alp/services/graphstream/PaperCoordinateGiver.java
@@ -54,91 +54,21 @@ public class PaperCoordinateGiver {
 		determineZ(root);
 
 		// Normalize X
-		Stack<Paper> papers = new Stack<>();
-		papers.push(root);
 
-		while(!papers.isEmpty()) {
-			var current = papers.pop();
-			if(current.getTitle() == null) continue;
-			current.setX(current.getX() - smallestYear);
-
-			if(current.getReferences() != null)
-				current.getReferences().stream()
-						.filter((Paper paper) -> Objects.nonNull(paper) && paper.getTitle() != null).collect(Collectors.toList())
-						.forEach(papers::push);
-		}
 	}
 
 	private Paper root;
 
 	private Integer determineX(ArrayList<Paper> papers, ArrayList<Paper> visited) {
-		// Smallest year is always root if there are no nodes to the left is invalid bc crossref D:
-		if(root == null) root = papers.get(0);
-		final int[] smallestYear = {LocalDate.now().getYear()};
-		if(papers == null) return smallestYear[0];
 
-		papers.forEach(paper -> {
-
-			if(paper.getTitle() == null || visited.contains(paper)) {
-				return;
-			}
-			visited.add(paper);
-
-			LocalDate publishingDate = LocalDate.of(
-					paper.getYear(),
-					paper.getMonth(),
-					paper.getDay());
-
-			smallestYear[0] = Math.min(smallestYear[0], publishingDate.getYear());
-
-			float daysInYear = (float) DateService.daysInYear(publishingDate);
-			float daysSinceBeginningOfYear = (float) DateService.daysSinceBeginningOfYear(publishingDate);
-
-			float distance = (float) (publishingDate.getYear()) + (daysSinceBeginningOfYear / daysInYear);
-			paper.setX(distance);
-
-			smallestYear[0] = Math.min(smallestYear[0], determineX(paper.getReferences(), visited));
-		});
-		return smallestYear[0];
 	}
 
 	private void determineY(Paper root) {
-		if(root == null || root.getTitle() == null) return;
 
-		determineYAlgo(root, defaultY);
 	}
 
 	private void determineYAlgo(Paper root, float height) {
-		if(root == null || root.getTitle() == null) return;
 
-		ArrayList<Paper> right = new ArrayList<>();
-		ArrayList<Paper> left = new ArrayList<>();
-
-		root.setY(height);
-
-		for(Paper reference : root.getReferences()) {
-			if(reference.compareTo(root) < 1) {
-				right.add(reference);
-			}
-			else {
-				left.add(reference);
-			}
-		}
-
-		float initialHeight = height;
-
-		right = PaperService.sortPapersByDecreasingYear(right);
-		left = PaperService.sortPapersByIncreasingYear(left);
-
-		for(Paper paper : right) {
-			paper.setY(height--);
-		}
-
-		height = initialHeight;
-
-		for(Paper paper : left) {
-			paper.setY(height--);
-		}
 	}
 
 	private void determineZ(Paper root) {
